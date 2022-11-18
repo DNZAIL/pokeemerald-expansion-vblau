c181b0ec024f5cc0a399d00169d66109e8460dc9
DNZAIL <dnzhang@uci.edu>
2022-11-18 14:24:32 -0800

    Added Starters + fixed bug with New Game

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -117,9 +117,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_DREEPY,
+    SPECIES_BELDUM,
+    SPECIES_CLEFFA,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
